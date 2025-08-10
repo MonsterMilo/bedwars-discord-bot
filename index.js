12a92d158c07d90fdb612b510931ca49651604c3
MonsterMilo <108966405+MonsterMilo@users.noreply.github.com>
2025-08-10 16:32:22 -0700

    fetch fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// At the very top of index.js
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Bot is running!"));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Web server running");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
 });
 
 const { Client, GatewayIntentBits } = require('discord.js');
