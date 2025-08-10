ffec77649789130c8ed927c53b26038c966b785c
MonsterMilo <108966405+MonsterMilo@users.noreply.github.com>
2025-08-10 16:24:34 -0700

    add fake web

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// At the very top of index.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Bot is running!"));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server running");
+});
+
 const { Client, GatewayIntentBits } = require('discord.js');
 const fetch = require('node-fetch');
 
